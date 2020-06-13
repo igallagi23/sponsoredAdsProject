8b56eaad0dcce4a655579be8ce7277968426c743
igallagi23 <32838995+igallagi23@users.noreply.github.com>
2020-06-13 15:12:55 +0300

    pull template spring boot project from spring io

---
+++ src/main/java/com/sponsoredAdsProject/SponsoredAdsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sponsoredAdsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsoredAdsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SponsoredAdsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sponsoredAdsProject/SponsoredAdsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sponsoredAdsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsoredAdsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
